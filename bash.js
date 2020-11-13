814a009eaf07e6e6d1efd7c0ed42c8ac71a7df0a
Ruchibrata Kundu <ruchibrata@rediffmail.com>
2020-11-13 15:06:12 -0500

    before module part

---
+++ bash.js
@@ -0,0 +1,10 @@
+process.stdout.write("prompt >");
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    process.stdout.write(process.cwd());
+  } else {
+    process.stdout.write("you typed" + cmd);
+    process.stdout.write("\nprompt > ");
+  }
+});
